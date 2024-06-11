2c73f8c823eabd08f4bbbd54819e1e6df543e37e
amaya-samirah <amaya.s.26292@gmail.com>
2024-06-11 13:52:02 -0400

    Updates to scenes

---
+++ supersciguinew/src/main/java/controllers/CreateAccountController.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import model.SuperSci;
+import model.UserList;
+import supersci.gui.App;
+
+public class CreateAccountController {
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private AnchorPane login_pane;
+
+    @FXML
+    private TextField txt_email;
+
+    @FXML
+    private TextField txt_password;
+
+    @FXML
+    private TextField txt_phoneNumber;
+
+    @FXML
+    private TextField txt_username;
+
+    @FXML
+    void createAccount(ActionEvent event) {
+        String username = txt_username.getText();
+        String password = txt_password.getText();
+        String phoneNumber = txt_phoneNumber.getText();
+        String email = txt_email.getText();
+        
+        UserList users = UserList.getInstance();
+        SuperSci superSci = new SuperSci(users);
+        boolean ifCreateAccount = superSci.createAccount(username, password, phoneNumber, email);
+    }
+
+    @FXML
+    void login(ActionEvent event) throws  IOException {
+        App.setRoot("login");
+    }
+
+}
