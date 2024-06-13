56fe9d2a08082ea1939d74d2063b1dd4dc21d0e8
amaya-samirah <amaya.s.26292@gmail.com>
2024-06-13 12:52:08 -0400

    fixed play screen

---
+++ supersciguinew/src/main/java/controllers/PlayScreenController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import supersci.gui.App;
+
+public class PlayScreenController {
+
+    @FXML
+    private Button Play_Button;
+
+    @FXML
+    private Label Uname_TextBox;
+
+    @FXML
+    void SwitchToBoard(ActionEvent event) throws IOException {
+        App.setRoot("board");
+    }
+
+}
